0c6fa7367540473d09bed1f88f2d35f3da833b0a
Osaid <osaid03>
2025-04-07 02:47:04 +0400

    feat/AH-7:integrate docker with AI Honeypot

--- app.py
+++ app.py
@@ -225,4 +225,4 @@ def geo_lookup(ip):
     
 if __name__ == '__main__':
     print("🚦 Flask app running...")
-    app.run(debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
